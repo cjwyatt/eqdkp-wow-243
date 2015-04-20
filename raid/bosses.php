cc8517166c15e3902a3ec7da074b08a3ae1f4125
Christopher Wyatt <chris@fourcore-computing.co.uk>
2015-04-20 06:59:59 +0100

    Creates initial game module.

---
+++ raid/bosses.php
@@ -0,0 +1,57 @@
+<?php
+/*	Project:	EQdkp-Plus
+ *	Package:	World of Warcraft game package
+ *	Link:		http://eqdkp-plus.eu
+ *
+ *	Copyright (C) 2006-2015 EQdkp-Plus Developer Team
+ *
+ *	This program is free software: you can redistribute it and/or modify
+ *	it under the terms of the GNU Affero General Public License as published
+ *	by the Free Software Foundation, either version 3 of the License, or
+ *	(at your option) any later version.
+ *
+ *	This program is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU Affero General Public License for more details.
+ *
+ *	You should have received a copy of the GNU Affero General Public License
+ *	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+if ( !defined('EQDKP_INC') ){
+	header('HTTP/1.0 404 Not Found');exit;
+} 
+
+$wow_bosses = array (
+	
+	'burning_crusade' => array(
+		'1068' => 0, #Siege über Keli'dan den Zerstörer (Blutkessel)
+		'1069' => 0, #Siege über Nexusprinz Shaffar (Managruft)
+		'1070' => 0, #Siege über den Epochenjäger (Flucht von Durnholde)
+		'1071' => 0, #Siege über Quagmirran (Sklavenunterkünfte)
+		'1072' => 0, #Siege über die Schattenmutter (Tiefensumpf)
+		'1073' => 0, #Siege über Exarch Maladaar (Auchenaikrypta)
+		'1074' => 0, #Siege über Klauenkönig Ikiss (Sethekkhallen)
+		'1075' => 0, #Siege über Murmur (Schattenlabyrinth)
+		'1076' => 0, #Siege über Aeonus (Öffnung des Dunklen Portals)
+		'1077' => 0, #Siege über Kriegsherr Kalithresh (Dampfkessel)
+		'1078' => 0, #Siege über Kargath Messerfaust (Zerschmetterte Hallen)
+		'1079' => 0, #Siege über Pathaleon den Kalkulator (Die Mechanar)
+		'1080' => 0, #Siege über Warpzweig (Die Botanika)
+		'1081' => 0, #Siege über Herold Horizontiss (Die Arkatraz)
+		'1082' => 0, #Siege über Kael'thas Sonnenwanderer (Terrasse der Magister)
+		'1083' => 0, #Siege über Prinz Malchezaar (Karazhan)
+		'1084' => 0, #Siege über Zul'jin (Zul'Aman)
+		'1085' => 0, #Siege über Gruul (Gruuls Unterschlupf)
+		'1086' => 0, #Siege über Magtheridon (Magtheridons Kammer)
+		'1087' => 0, #Siege über Lady Vashj (Höhle des Schlangenschreins)
+		'1088' => 0, #Siege über Kael'thas Sonnenwanderer (Festung der Stürme)
+		'1089' => 0, #Siege über Illidan Sturmgrimm (Der Schwarze Tempel)
+		'1090' => 0, #Siege über Kil'jaeden (Sonnenbrunnenplateau)
+	),	
+); 
+
+
+$wow_instances = array ('burning_crusade');
+?>
